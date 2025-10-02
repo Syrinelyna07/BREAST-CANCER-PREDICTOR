2065488048e343e97aa5b7d95f5fbbcb56ed3138
syrinelyna07 <ns_zouak@esi.dz>
2025-10-02 10:37:17 +0100

    submission

---
+++ model/mymain.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def get_clean_data():
+    data= pd.read_csv("data/data.csv")
+    
+    data = data.drop(['Unnamed: 32', 'id'],axis=1)
+    data['diagnosis']= data ['diagnosis'].map({'M':1 , 'B':0})
+    return data 
+
+def main():
+    data = get_clean_data()
+    print(data.info())
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
